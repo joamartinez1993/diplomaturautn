758352ebcd4bd1ad74428486d9b6aed3de6070ac
Joaquin Martinez <joaquin.martinez1993@gmail.com>
2022-05-02 17:43:42 -0300

    frontend novedades

--- M4U1/frontend/src/pages/NovedadesPage.js
+++ M4U1/frontend/src/pages/NovedadesPage.js
@@ -1,6 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import NovedadItem from './NovedadItem';
+
 const NovedadesPage = (props) => {
+
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState ([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading (true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+        cargarNovedades();
+    }, []);
+
     return(
-        <div>NovedadesPage</div>
+       <section className='holder'>
+           <h2>Novedades</h2>
+           {loading ? (
+               <p>Cargando...</p>
+           ) : (
+               novedades.map(item => <NovedadItem key={item.id}
+                title = {item.titulo} subtitle = {item.subtitulo}
+                imagen={item.imagen} body={item.cuerpo} />)
+           )}
+           </section>
     );
 } 
 
